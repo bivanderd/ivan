ce4c064c9532fee736bdccd5f2846817696e581b
PRODOK <sragenivan29@gmail.com>
2022-04-28 22:45:10 +0700

    Portfolio Ivan

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  content: ['./components/**/*.{html,js}',
+  './pages/**/*.{html,js}',
+  '.public/css/index.html',],
+  theme:{
+   
+   
+    extend: {
+
+      colors: {
+        utama: '#14b8a6',
+        sekunder: '#0f172a',
+      },
+
+      fontFamily: {
+
+        bebas: ['Bebas Neue']
+        ,lato: ['Lato'],
+        roboto: ['Roboto']
+    
+      }
+      
+
+
+
+
+
+    },
+  },
+  plugins: [],
+}
